3e1e69927bd679826f92d3e305766a9713dfa878
Emil Cebotarosh <emil.cebotarosh@gmail.com>
2021-05-16 18:24:41 +0300

    Implemented Getter Modules

---
+++ Modules/Modules/Modules/GetClipboardTextModule.cs
@@ -0,0 +1,22 @@
+﻿namespace Modules
+{
+    class GetClipboardTextModule : GetterModule
+    {
+        public override void Execute()
+        {
+            if (System.Windows.Forms.Clipboard.ContainsText())
+            {
+                _data = System.Windows.Forms.Clipboard.GetText();
+            }
+            else
+            {
+                throw new System.Exception("Clipboard does not contain text!");
+            }
+        }
+
+        public override object GetData()
+        {
+            return _data;
+        }
+    }
+}
---
+++ Modules/Modules/Modules/Interfaces/ISetterModule.cs
@@ -0,0 +1,6 @@
+﻿namespace Modules
+{
+    interface ISetterModule : IModule
+    {
+    }
+}
