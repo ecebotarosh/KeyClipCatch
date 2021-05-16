7bd771141756b9a88a2b5f995e8a03e92b147260
Emil Cebotarosh <emil.cebotarosh@gmail.com>
2021-05-16 19:13:53 +0300

    Added Generic Setter Module

--- Modules/Modules/Modules/Generic Modules/SetterModule.cs
+++ Modules/Modules/Modules/Generic Modules/SetterModule.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Modules.Generic_Modules
+﻿namespace Modules.Generic_Modules
 {
     abstract class SetterModule : IModule
     {
